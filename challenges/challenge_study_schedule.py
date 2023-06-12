0afb95aacc06ca0ba476ccad036461b8174af54c
Alissa Maximo <alissahiratamaximo@gmail.com>
2023-06-12 20:43:20 -0300

    Req 1 completo

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,14 @@
 def study_schedule(permanence_period, target_time):
     """Faça o código aqui."""
-    raise NotImplementedError
+    # Requisito 1
+
+    if type(target_time or permanence_period) != int:
+        return None
+
+    total_present_students = 0
+
+    for entrance_time, exit_time in permanence_period:
+        if entrance_time <= target_time <= exit_time:
+            total_present_students += 1
+
+    return total_present_students
