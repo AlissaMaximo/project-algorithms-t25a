81b2815dbd0036d7dfddcbf2b757cbbefa9e4a02
Alissa Maximo <alissahiratamaximo@gmail.com>
2023-06-13 02:56:11 -0300

    Req4 falta passar um teste de strings vazias

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,3 +1,25 @@
 def is_anagram(first_string, second_string):
     """Faça o código aqui."""
-    raise NotImplementedError
+
+    def quicksort(list):
+        if len(list) <= 1:
+            return list
+
+        # se a lista for ["o", "i"], pivot vai ser "i"
+        pivot = list[len(list) // 2]
+
+        left = [char for char in list if char < pivot]  # entra "o"
+
+        middle = [char for char in list if char == pivot]  # entra "i"
+
+        # não entra item da lista
+        right = [char for char in list if char > pivot]
+
+        return quicksort(left) + middle + quicksort(right)
+
+    sorted_lowered_str1 = quicksort(list(first_string.lower()))
+    sorted_lowered_str2 = quicksort(list(second_string.lower()))
+
+    return ("".join(sorted_lowered_str1),
+            "".join(sorted_lowered_str2),
+            sorted_lowered_str1 == sorted_lowered_str2)
