0f7534df7a9166b1986d0fb5d8a1de35fc9bb0b0
Alissa Maximo <alissahiratamaximo@gmail.com>
2023-06-12 21:55:44 -0300

    Req 2 completo

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,5 +1,16 @@
-# from challenges.challenge_encrypt_message import encrypt_message
+from challenges.challenge_encrypt_message import encrypt_message
+import pytest
 
 
 def test_encrypt_message():
-    pass
+    with pytest.raises(TypeError, match="tipo inválido para message"):
+        encrypt_message(6, 4)
+
+    with pytest.raises(TypeError, match="tipo inválido para key"):
+        encrypt_message("message", "invalidkey")
+
+assert encrypt_message("kuroneko", 3) == "ruk_okeno"
+
+assert encrypt_message("kuroneko", 6) == "oken_oruk"
+
+assert encrypt_message("kuroneko", 9) == "okenoruk"
